cfb615eb24d5c91e31280396c58a669cbd0d30b5
CaioViveiros <caio.soares@sptech.school>
2024-04-28 21:06:11 -0300

    Adicionado validação de campos no cadastro

--- Website/Cadastro/script.js
+++ Website/Cadastro/script.js
@@ -1,11 +1,17 @@
 function entrar() {
+    const usuario = input_usuario.value
     const email = input_email.value
     const senha = input_senha.value
+    const confirmacao = input_confirmacao.value
 
-    if (email.indexOf('@') == -1) {
-        alert('Erro')
+    if (usuario == '' || email == '' || senha == '' || confirmacao == '') {
+        alert('Preencha todos os campos para criar sua conta')
+    } else if (email.indexOf('@') == -1) {
+        alert('insira um e-mail válido')
+    } else if (senha != confirmacao) {
+        alert('As senhas estão diferentes')
     } else {
-        alert('Entrou')
+        alert('Usuário cadastrado com sucesso')
     }
 }
 
