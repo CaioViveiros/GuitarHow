29ece9c440ba621f91e9c3bddb546e2ae503775a
CaioViveiros <caio.soares@sptech.school>
2024-05-08 23:42:58 -0300

    Adicionado - Pagina de bem-vindo

--- Website/Cadastro/script.js
+++ Website/Cadastro/script.js
@@ -1,18 +1,5 @@
 function entrar() {
-    const usuario = input_usuario.value
-    const email = input_email.value
-    const senha = input_senha.value
-    const confirmacao = input_confirmacao.value
-
-    if (usuario == '' || email == '' || senha == '' || confirmacao == '') {
-        alert('Preencha todos os campos para criar sua conta')
-    } else if (email.indexOf('@') == -1) {
-        alert('insira um e-mail válido')
-    } else if (senha != confirmacao) {
-        alert('As senhas estão diferentes')
-    } else {
-        alert('Usuário cadastrado com sucesso')
-    }
+    window.location.href = '../Bem-vindo/index.html'
 }
 
 function irParaLogin() {
