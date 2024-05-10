14ee4d07c277de791b85d9860805c57f04563957
CaioViveiros <caio.soares@sptech.school>
2024-05-10 15:11:50 -0300

    Alteração - Organização de pastas

---
+++ Public/Js/link.js
@@ -0,0 +1,18 @@
+function irParaLogin() {
+    window.location.href = '../Html/login.html'
+}
+function irParaCadastro() {
+    window.location.href = '../Html/cadastro.html'
+}
+function irParaHome() {
+    window.location.href = '../Html/website.html'
+}
+function irParaBemVindo() {
+    window.location.href = '../Html/bem-vindo.html'
+}
+function irParaLogin() {
+    window.location.href = '../Html/login.html'
+}
+function irParaHome() {
+    window.location.href = '../Html/website.html'
+}
