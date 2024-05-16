723665589989855d6e7a7cb8667d6f0de68be65e
CaioViveiros <caio.soares@sptech.school>
2024-05-16 00:26:50 -0300

    Alteração - Validação de campos na tela de cadastro

--- Public/Js/cadastro.js
+++ Public/Js/cadastro.js
@@ -1,46 +1,100 @@
-function Cadastrar() {
+function cadastrar() {
     const usuario = input_usuario.value
     const email = input_email.value
     const senha = input_senha.value
-    const confirmacao_senha = input_confirmacao_senha.value
+    const confirmacaoSenha = input_confirmacao_senha.value
 
-    const erro_campo_vazio = 'Por favor, preencha todos os campos para realizar o cadastro'
-    const erro_email_invalido = 'Por favor, insira um E-mail válido para realizar o cadastro'
+    const erroCampoVazio = 'Por favor, preencha todos os campos para realizar o cadastro'
 
     if (usuario == '') {
-        mensagem_alerta.innerHTML = erro_campo_vazio
-        Alertar()
+        mensagem_alerta.innerHTML = erroCampoVazio
+        alertar()
 
     } else if (email == '') {
-        mensagem_alerta.innerHTML = erro_campo_vazio
-        Alertar()
+        mensagem_alerta.innerHTML = erroCampoVazio
+        alertar()
 
     } else if (senha == '') {
-        mensagem_alerta.innerHTML = erro_campo_vazio
-        Alertar()
-        
-    } else if (confirmacao_senha == '') {
-        mensagem_alerta.innerHTML = erro_campo_vazio
-        Alertar()
+        mensagem_alerta.innerHTML = erroCampoVazio
+        alertar()
 
-    } else if (email.indexOf('@') == -1){
-        mensagem_alerta.innerHTML = erro_email_invalido
-        Alertar()
+    } else if (confirmacaoSenha == '') {
+        mensagem_alerta.innerHTML = erroCampoVazio
+        alertar()
+
+    } else if (email.indexOf('@') == -1 || email.indexOf('.') == -1) {
+        mensagem_alerta.innerHTML = 'Por favor, insira um E-mail válido para realizar o cadastro'
+        alertar()
 
     } else {
 
-        // Validação de senha
-        
+        const tamanhoSenha = senha.length
+
+        if (tamanhoSenha < 6) {
+            mensagem_alerta.innerHTML = 'Sua senha deve ter no minimo 6 caracteres'
+            alertar()
+
+        } else {
+            let temMaiuscula = false
+            let temMinuscula = false
+            let temNumero = false
+            let temCaractereEspecial = false
+            let senhasIguais = false
+            const listaCaracteresEspeciais = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '?']
+
+            for (let posicao = 0; posicao < tamanhoSenha; posicao++) {
+                let caractere = senha[posicao]
+
+                if (caractere >= 'A' && caractere <= 'Z') {
+                    temMaiuscula = true
+                }
+                if (caractere >= 'a' && caractere <= 'z') {
+                    temMinuscula = true
+                }
+                if (caractere >= '0' && caractere <= '9') {
+                    temNumero = true
+                }
+                if (listaCaracteresEspeciais.indexOf(caractere) != -1) {
+                    temCaractereEspecial = true
+                }
+            }
+
+            if (temMaiuscula == false) {
+                mensagem_alerta.innerHTML = 'Sua senha deve ter no minimo uma letra maiuscula'
+                alertar()
+            }
+            if (temMinuscula == false) {
+                mensagem_alerta.innerHTML = 'Sua senha deve ter no minimo uma letra minuscula'
+                alertar()
+            }
+            if (temNumero == false) {
+                mensagem_alerta.innerHTML = 'Sua senha deve ter no minimo um numero'
+                alertar()
+            }
+            if (temCaractereEspecial == false) {
+                mensagem_alerta.innerHTML = 'Sua senha deve ter no minimo um caracter especial'
+                alertar()
+            }
+
+            if (temMaiuscula && temMinuscula && temNumero && temCaractereEspecial) {
+                if (senha != confirmacaoSenha) {
+                    mensagem_alerta.innerHTML = 'Suas senhas devem ser iguais'
+                    alertar()
+                } else {
+                    irParaBemVindo()
+                }
+            }
+        }
     }
 }
 
-function Alertar() {
+function alertar() {
     const alerta = document.getElementById('div_alerta')
 
     alerta.classList.remove('display-hidden')
     alerta.classList.add('alerta')
 }
-function FecharAlerta() {
+function fecharAlerta() {
     const alerta = document.getElementById('div_alerta')
 
     alerta.classList.remove('alerta')
---
+++ Public/Js/login.js
@@ -0,0 +1,33 @@
+function entrar() {
+    const email = input_email.value
+    const senha = input_senha.value
+
+    const erroCampoVazio = 'Por favor, preencha todos os campos para realizar o cadastro'
+
+    if (email == '') {
+        mensagem_alerta.innerHTML = erroCampoVazio
+        alertar()
+
+    } else if (senha == '') {
+        mensagem_alerta.innerHTML = erroCampoVazio
+        alertar()
+
+    } else if (email.indexOf('@') == -1 || email.indexOf('.') == -1) {
+        mensagem_alerta.innerHTML = 'Por favor, insira um E-mail válido para realizar o cadastro'
+        alertar()
+    }
+}
+
+function alertar() {
+    const alerta = document.getElementById('div_alerta')
+
+    alerta.classList.remove('display-hidden')
+    alerta.classList.add('alerta')
+}
+function fecharAlerta() {
+    const alerta = document.getElementById('div_alerta')
+
+    alerta.classList.remove('alerta')
+    alerta.classList.add('display-hidden')
+}
+
