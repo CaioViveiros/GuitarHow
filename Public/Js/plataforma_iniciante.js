ebe6644d202651b9b020bc31c15f3477ca3ad1a5
CaioViveiros <caio.soares@sptech.school>
2024-05-19 13:59:14 -0300

    Atualização - WebSite

--- Public/Js/plataforma_iniciante.js
+++ Public/Js/plataforma_iniciante.js
@@ -10,3 +10,47 @@ function fecharAlerta() {
     alerta.classList.remove('alerta-conteudi')
     alerta.classList.add('display-hidden')
 }
+
+function mostrarHome() {
+    div_sessao_home.style.display = 'flex'
+    div_sessao_conteudo.style.display = 'none'
+    div_sessao_pratica.style.display = 'none'
+    div_sessao_progressao.style.display = 'none'
+}
+
+function mostrarConteudo() {
+    div_sessao_home.style.display = 'none'
+    div_sessao_conteudo.style.display = 'flex'
+    div_sessao_pratica.style.display = 'none'
+    div_sessao_progressao.style.display = 'none'
+}
+
+function mostrarPratica() {
+    div_sessao_home.style.display = 'none'
+    div_sessao_conteudo.style.display = 'none'
+    div_sessao_pratica.style.display = 'flex'
+    button_inicio.style.display = 'flex'
+    div_box_teste.style.display = 'none'
+    div_sessao_progressao.style.display = 'none'
+}
+
+function iniciarTeste() {
+    button_inicio.style.display = 'none'
+    div_box_teste.style.display = 'flex'
+    numeroQuestao = 1
+    div_numero_questao.innerHTML = numeroQuestao
+}
+
+let numeroQuestao = 1
+function proximaPergunta() {
+    
+    if (numeroQuestao < 10) {
+        numeroQuestao += 1
+        div_numero_questao.innerHTML = numeroQuestao
+    } else {
+        alertar()
+        numeroQuestao = 1
+        mostrarPratica()
+    }
+}
+
